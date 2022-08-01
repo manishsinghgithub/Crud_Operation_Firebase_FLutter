5df74389d76262021e2840a73d2d2b735e46129c
Manish <manishsinghonline2@gmail.com>
2022-08-01 23:54:30 +0530

    finish application

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'dart:ffi';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:crud_firebase/firebaseFirestoreService.dart';
+import 'package:crud_firebase/modalViewForUpdate.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,18 +13,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final CollectionReference _firebaseFirestore =
+      FirebaseFirestore.instance.collection("pruduct");
+
   final FireBaseFirestoreServices fireBaseFirestoreServices =
       FireBaseFirestoreServices();
+  final ModalService _modalService = ModalService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home "),
+        title: const Text("Item List "),
         centerTitle: true,
         actions: [
           TextButton(
-              onPressed: () {},
+              onPressed: () async {
+                await _modalService.addModalService(context);
+                setState(() {});
+              },
               child: const Text(
                 "Add more ",
                 style: TextStyle(color: Colors.white),
@@ -31,7 +39,7 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: StreamBuilder(
-        stream: fireBaseFirestoreServices.getData(),
+        stream: _firebaseFirestore.snapshots(),
         builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
           if (streamSnapshot.hasData) {
             return ListView.builder(
@@ -45,7 +53,11 @@ class _HomePageState extends State<HomePage> {
                     title: Text(documentSnapshot["name"]),
                     subtitle: Text(documentSnapshot["price"].toString()),
                     leading: IconButton(
-                      onPressed: () async {},
+                      onPressed: () async {
+                        await _modalService.updateModalService(
+                            context, documentSnapshot);
+                        setState(() {});
+                      },
                       icon: const Icon(
                         Icons.upload,
                         color: Colors.teal,
@@ -55,6 +67,7 @@ class _HomePageState extends State<HomePage> {
                       onPressed: () async {
                         await fireBaseFirestoreServices
                             .deleteData(documentSnapshot);
+                        setState(() {});
                       },
                       icon: const Icon(
                         Icons.delete,
--- lib/modalViewForUpdate.dart
+++ lib/modalViewForUpdate.dart
@@ -14,12 +14,16 @@ class ModalService {
       showModalBottomSheet(
           context: context,
           builder: (BuildContext ctext) {
-            return Container(
-              child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
+            return Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Item Name:",
+                        ),
                         validator: (value) {
                           if (value == null || value.isEmpty) {
                             return "Item name Required!";
@@ -27,7 +31,13 @@ class ModalService {
                           title = value.toString();
                         },
                       ),
-                      TextFormField(
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Item price:",
+                        ),
                         validator: (value) {
                           if (value == null || value.isEmpty) {
                             return "Price Required!";
@@ -35,30 +45,44 @@ class ModalService {
                           price = int.parse(value);
                         },
                       ),
-                      MaterialButton(
-                        onPressed: () async {
-                          if (_formKey.currentState!.validate()) {
-                            await fireBaseFirestoreServices.updateData(
-                                documentSnapshot, title, price);
-                          }
-                        },
-                        child: Text("Update"),
-                      )
-                    ],
-                  )),
-            );
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    MaterialButton(
+                      color: Colors.teal,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18)),
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          await fireBaseFirestoreServices.updateData(
+                              documentSnapshot, title, price);
+                        }
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        "Update",
+                        style: TextStyle(color: Colors.white, fontSize: 22),
+                      ),
+                    )
+                  ],
+                ));
           });
 
   Future<Container?> addModalService(BuildContext context) =>
       showModalBottomSheet(
           context: context,
           builder: (BuildContext ctext) {
-            return Container(
-              child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
+            return Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Name of item",
+                        ),
                         validator: (value) {
                           if (value == null || value.isEmpty) {
                             return "Item name Required!";
@@ -66,7 +90,13 @@ class ModalService {
                           title = value.toString();
                         },
                       ),
-                      TextFormField(
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: "Price of item",
+                        ),
                         validator: (value) {
                           if (value == null || value.isEmpty) {
                             return "Price Required!";
@@ -74,17 +104,28 @@ class ModalService {
                           price = int.parse(value);
                         },
                       ),
-                      MaterialButton(
-                        onPressed: () async {
-                          if (_formKey.currentState!.validate()) {
-                            await fireBaseFirestoreServices.addData(
-                                title, price);
-                          }
-                        },
-                        child: Text("Update"),
-                      )
-                    ],
-                  )),
-            );
+                    ),
+                    const SizedBox(height: 10),
+                    MaterialButton(
+                      color: Colors.teal,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          await fireBaseFirestoreServices.addData(title, price);
+                        }
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        "Add Data",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                        ),
+                      ),
+                    )
+                  ],
+                ));
           });
 }
